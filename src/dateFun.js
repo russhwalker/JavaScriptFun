b541e4b9b219b218278ef62a70c23d5b2fb5de37
russell walker <russ.h.walker@gmail.com>
2018-02-28 14:44:06 -0500

    cleanup

--- src/dateFun.js
+++ src/dateFun.js
@@ -2,43 +2,46 @@ CoreFun.PrintHeading('dateFun.js');
 
 (function () {
 
-    CoreFun.Print('Current Dates', 'strong');
+    CoreFun.Print('Current Dates');
     CoreFun.Print(new Date());
     CoreFun.Print(new Date().toUTCString());
-    CoreFun.Print();
+    CoreFun.PrintLine();
 
-    CoreFun.Print('Date Parsing', 'strong');
+    CoreFun.Print('Date Parsing');
     var d = '02/28/2017 12:30:00';
-    CoreFun.Print('Raw Date: ' + d, 'strong');
+    CoreFun.Print('Raw Date: ' + d);
     CoreFun.Print(new Date(d));
     CoreFun.Print(new Date(2017, 1, 28, 12, 30));
     CoreFun.Print(new Date(d).toDateString());
     CoreFun.Print(new Date(d).toLocaleString());
     CoreFun.Print(new Date(d).toLocaleDateString());
     CoreFun.Print(new Date(d).toLocaleTimeString());
+    CoreFun.PrintLine();
 
     d = '2017-02-28T12:30:00';
-    CoreFun.Print('Raw Date: ' + d, 'strong');
+    CoreFun.Print('Raw Date: ' + d);
     CoreFun.Print(new Date(d));
     CoreFun.Print(new Date(d).toLocaleString());
+    CoreFun.PrintLine();
 
     d = '2017-02-28T12:30:00Z';
-    CoreFun.Print('Raw Date: ' + d, 'strong');
+    CoreFun.Print('Raw Date: ' + d);
     CoreFun.Print(new Date(d));
     CoreFun.Print(new Date(d).toLocaleString());
+    CoreFun.PrintLine();
 
     d = '2017-02-28T12:30:00+0000';
-    CoreFun.Print('Raw Date: ' + d, 'strong');
+    CoreFun.Print('Raw Date: ' + d);
     CoreFun.Print(new Date(d));//Invalid in IE 11
     CoreFun.Print(new Date(d).toLocaleString());//Invalid in IE 11
-    CoreFun.Print();
+    CoreFun.PrintLine();
 
-    CoreFun.Print('Ellapsed Time', 'strong');
+    CoreFun.Print('Ellapsed Time');
     var startDate = new Date(2017, 1, 1, 8);
     var endDate = new Date(2017, 1, 1, 11);
     var ellapsedTimeInMilliseconds = endDate - startDate;
     CoreFun.Print(ellapsedTimeInMilliseconds + ' milliseconds');
     CoreFun.Print(Math.floor(ellapsedTimeInMilliseconds / 3600000) + ' hours');
-    CoreFun.Print();
+    CoreFun.PrintLine();
 
 })();
